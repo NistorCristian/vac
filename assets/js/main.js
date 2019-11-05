2add050e6a835526f18b5bf0c7b34a75c184f969
Nistor Cristian <contact@nistorcristian.com>
2019-11-06 00:28:21 +0200

    Improved home
    Signed-off-by: Nistor Cristian <contact@nistorcristian.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,19 @@
 jQuery(document).ready(function ($) {
     
-    console.log('start');
+    var swyperFeatures = new Swiper ('.features', {
+        simulateTouch: false,
+        pagination: {
+            el: '.features .swiper-pagination',
+            type: 'bullets',
+        },
+    })
+
+    var swiperTable = new Swiper ('.table-holder', {
+        simulateTouch: false,
+        pagination: {
+            el: '.table-holder .swiper-pagination',
+            type: 'bullets',
+        },
+    })
 
 });
